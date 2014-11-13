429b39a7090710bdb2c4977feeaef2623b12ad83
App Academy Student <appacademy@Apps-Mac-mini-14.local>
2014-11-13 09:38:18 -0500

    add flash errors, adjusted height

--- app/controllers/contact_mailers_controller.rb
+++ app/controllers/contact_mailers_controller.rb
@@ -1,7 +1,7 @@
 class ContactMailersController < ApplicationController
   def deliver_contact
     ContactMailer.contact_email(params[:email]).deliver
-    flash[:notice] = ["Your message has been sent!"]
+    flash[:notice] = "Your message has been sent!"
     redirect_to pages_url
   end
 end
