f951c54a553ec3dad9fa3d32617208e80639e0bc
App Academy Student <appacademy@Apps-Mac-mini-28.local>
2014-11-14 13:27:54 -0500

    add styling to chat

--- todo.rb
+++ todo.rb
@@ -11,65 +11,65 @@
 Detail:
 
  Resume:
-    -create resume on google docs
-    -direct links to github and linked in
-    -add the features you built and the problems you solved to build them.
-    -Try to think of at least two cool solutions you came up with for each project.
-    -list projects first
-      -asteroids
-      -Kickstarter clone
-        -write about schema choices with users and user bio
-        -write about storing previous url in cookie.
-        -overrode parse method
-      -chess
-        -does chess make a deep dup?
-        -inherits classes
-        -make sure you have checkmate
-    -list skils
-        - Ruby
-        - Ruby on Rails
-        - JavaScript
-        - jQuery
-        - Backbone.js
-        - SQL
-        - Git
-        - HTML
-        - CSS
-    -work histroy
-      -wirte results of work
-      -fourcorners
-        -took opertunity to start a buisness - what was the resutl
-        -made static html websites - what was the resutl
-      -cloud 9
-        -created inhouse delivery saving on outside delivery. hired another person - what was the resutl
-        -created in house customer service systmes for returning mattress - what was the resutl
-    -education
-      -appacademy
-        -App Academy''s admissions rate is below 5% and that the teaching emphasizes coding style and best practices.
-      -mum
+    # -create resume on google docs
+    # -direct links to github and linked in
+    # -add the features you built and the problems you solved to build them.
+    # -Try to think of at least two cool solutions you came up with for each project.
+    # -list projects first
+    #   -asteroids
+    #   -Kickstarter clone
+    #     -write about schema choices with users and user bio
+    #     -write about storing previous url in cookie.
+    #     -overrode parse method
+    #   -chess
+    #     -does chess make a deep dup?
+    #     -inherits classes
+    #     -make sure you have checkmate
+    # -list skils
+    #     - Ruby
+    #     - Ruby on Rails
+    #     - JavaScript
+    #     - jQuery
+    #     - Backbone.js
+    #     - SQL
+    #     - Git
+    #     - HTML
+    #     - CSS
+    # -work histroy
+    #   -wirte results of work
+    #   -fourcorners
+    #     -took opertunity to start a buisness - what was the resutl
+    #     -made static html websites - what was the resutl
+    #   -cloud 9
+    #     -created inhouse delivery saving on outside delivery. hired another person - what was the resutl
+    #     -created in house customer service systmes for returning mattress - what was the resutl
+    # -education
+    #   -appacademy
+    #     -App Academy''s admissions rate is below 5% and that the teaching emphasizes coding style and best practices.
+    #   -mum
     -resume formatting
 
   Cover_letter:
-    -interviewers might ask you where you learned to code. At that point you want to give the full story.
-    Build a template with these classy touches:
-      - Have a minimally styled header that includes your contact information.
-      - Below that insert the date.
-      - Below that insert the name / title / company name / work address of the person you''re writing to.
-    -In the subject line, name the position you''re applying for and the city it''s in, separated by a dash.
-    -This way you can start off strong with a "I am a web developer with experience in X, Y and Z".
-    -Somewhere in your letter write two sentences about the company and why you want to work there.
-    -cover these points
-        -What are your skills?
-        -What is your background (if it is relevant)?
-        -What kind of company do you want to join? (Keep this generic.)
-        -What about this company matches that? (The one sentence you might change for each application.)
-        -Mention the materials you're sending along (résumé + github + one or two live links).
-
-        -Addressing the letter when you don't know the name of the person reading it:
-
-        -Use "Hello [company] team" if they''re a laidback company.
-        -Use "Dear Sir or Madam" if they seem stiff.
-    -Set up a signature with your mail client. Include the link to your portfolio site.
+    # -interviewers might ask you where you learned to code. At that point you want to give the full story.
+    # Build a template with these classy touches:
+    #   - Have a minimally styled header that includes your contact information.
+    #   - Below that insert the date.
+    #   - Below that insert the name / title / company name / work address of the person you''re writing to.
+    # -In the subject line, name the position you''re applying for and the city it''s in, separated by a dash.
+    # -This way you can start off strong with a "I am a web developer with experience in X, Y and Z".
+    # -Somewhere in your letter write two sentences about the company and why you want to work there.
+    # -cover these points
+    #     -What are your skills?
+    #     -What is your background (if it is relevant)?
+    #     -What kind of company do you want to join? (Keep this generic.)
+    #     -What about this company matches that? (The one sentence you might change for each application.)
+    #     -Mention the materials you're sending along (résumé + github + one or two live links).
+    #
+    #     -Addressing the letter when you don't know the name of the person reading it:
+    #
+    #     -Use "Hello [company] team" if they''re a laidback company.
+    #     -Use "Dear Sir or Madam" if they seem stiff.
+    # -Set up a signature with your mail client. Include the link to your portfolio site.
 
   UpdateLinkedIn:
     -linkedIn layout
